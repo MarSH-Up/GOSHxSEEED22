323f6eaa10fefd7805a71e49593a15a58123e53f
Mario De Los Santos <64880712+MarSH-Up@users.noreply.github.com>
2022-10-29 16:02:20 -0500

    Update 2022_GOSH_WioTerminal_Example.ino

--- 2022_GOSH_Song/2022_GOSH_WioTerminal_Example.ino
+++ 2022_GOSH_Song/2022_GOSH_WioTerminal_Example.ino
@@ -1,21 +1,50 @@
-
+/* Macro Define */
+#define BUZZER_PIN WIO_BUZZER /* sig pin of the buzzer */
+ 
+int length = 15;         /* the number of notes */
+char notes[] = "ccggaagffeeddc ";
+int beats[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 };
+int tempo = 300;
+ 
 void setup() {
-  Serial.begin(115200);
-  pinMode(WIO_KEY_A, INPUT_PULLUP);
-  pinMode(WIO_BUZZER, OUTPUT);
+    //set buzzer pin as output
+    pinMode(BUZZER_PIN, OUTPUT);
 }
  
 void loop() {
-   if (digitalRead(WIO_KEY_A) == LOW) {
-    Serial.println("A Key pressed");
-    analogWrite(WIO_BUZZER, 128);
-    delay(1000);
-    analogWrite(WIO_BUZZER, 0);
-    delay(1000);
-   }
-   delay(200);
+    for(int i = 0; i < length; i++) {
+        if(notes[i] == ' ') { 
+            delay(beats[i] * tempo);
+        } else {
+            playNote(notes[i], beats[i] * tempo);
+        }
+        delay(tempo / 2);    /* delay between notes */
+    }
+ 
+}
+ 
+
+//Functions
+void playTone(int tone, int duration) {
+    //For loops
+    for (long i = 0; i < duration * 1000L; i += tone * 2) {
+        digitalWrite(BUZZER_PIN, HIGH);
+        delayMicroseconds(tone);
+        digitalWrite(BUZZER_PIN, LOW);
+        delayMicroseconds(tone);
+    }
 }
 
-
+void playNote(char note, int duration) {
+    char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+    int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+ 
+    // play the tone corresponding to the note name
+    for (int i = 0; i < 8; i++) {
+        if (names[i] == note) {
+            playTone(tones[i], duration);
+        }
+    }
+}
 
 
